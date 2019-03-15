2d7349f92cb822a01635ab17aaf3687782218de1
donk condor <condordonkey@gmail.com>
2019-03-16 07:08:35 +0900

    finish ch09

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ attr_accessor :remember_token
     BCrypt::Password.create(string, cost: cost)
   end
 
-    # ランダムなトークンを返す
+  # ランダムなトークンを返す
   def User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -25,5 +25,16 @@ attr_accessor :remember_token
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-  end  
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
